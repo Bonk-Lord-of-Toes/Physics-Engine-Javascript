de644eac7a8f45780ee16476a81b0fe51a27a946
Bonk-Lord-of-Toes <152087172+Bonk-Lord-of-Toes@users.noreply.github.com>
2023-12-24 08:22:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-var balls = []
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-
-var newBall = (x,y,r,vx,vy) => {
-    balls.push({x:x,y:y,r:r,prevPos:[x-vx,y-vy]})
-}
-
-newBall(100,100,40,-5,5)
-newBall(500,500,40,5,5)
+var balls = [{x:100,y:100,r:30,prevPos:[100,100]},{x:130,y:130,r:30,prevPos:[130,130]}]
 
 var dist = (x,x1,y,y1) => {
     return Math.sqrt(((x-x1)**2)+((y-y1)**2))
@@ -18,43 +11,39 @@ var dist = (x,x1,y,y1) => {
 var main = () => {
     ctx.fillStyle = "black"
     ctx.fillRect(0,0,canvas.width,canvas.height)
-
     for(var i=0;i<balls.length;i++){
-    ctx.beginPath()
-    ctx.arc(balls[i].x,balls[i].y,balls[i].r,0,2*Math.PI)
-    ctx.fillStyle = "red"
-    ctx.fill()
+        ctx.beginPath()
+        ctx.arc(balls[i].x,balls[i].y,balls[i].r,0,2*Math.PI)
+        ctx.fillStyle = "red"
+        ctx.fill()
+        
+        //velocity calculations
+        var tempPos = [balls[i].x,balls[i].y]
 
-    var tempPos = [balls[i].x,balls[i].y]
-
-    if(balls[i].x<=balls[i].r||balls[i].x>canvas.width-balls[i].r){
+        if(balls[i].x<balls[i].r||balls[i].x>canvas.width-balls[i].r){
         balls[i].x-=(balls[i].x-balls[i].prevPos[0])
-    }
-    if(balls[i].y<=balls[i].r||balls[i].y>canvas.height-balls[i].r){
-        balls[i].y-=balls[i].y-balls[i].prevPos[1]
-    }
+        } else{
+        balls[i].x+=(balls[i].x-balls[i].prevPos[0])
+        }
+        if(balls[i].y<balls[i].r||balls[i].y>canvas.height-balls[i].r){
+        balls[i].y-=(balls[i].y-balls[i].prevPos[1])
+        } else{
+        balls[i].y+=(balls[i].y-balls[i].prevPos[1])
+        }
+        
+        balls[i].prevPos = tempPos
 
-    balls[i].x+=balls[i].x-balls[i].prevPos[0]
-    balls[i].y+=balls[i].y-balls[i].prevPos[1]
-    balls[i].prevPos = tempPos
 
-    for(var i1=0;i1<balls.length;i1++){
-        if(i1!=i){
-            var d = dist(balls[i].x,balls[i1].x,balls[i].y,balls[i1].y)
-            if(d<=balls[i].r+balls[i1].r){
-                if(balls[i].x!=balls[i1].x){
-                    var m = (balls[i].y-balls[i1].y)/(balls[i].x-balls[i1].x)
-                } else {
-                    var m = 0.1
+        for(var i1=0;i1<balls.length;i1++){
+            if(i1!=i){
+                var d = dist(balls[i].x,balls[i1].x,balls[i].y,balls[i1].y)
+                if(d<balls[i].r+balls[i1].r){
+                    balls[i].x+=(balls[i].x-balls[i1].x)/(d-balls[i].r+balls[i1].r)
+                    balls[i].y+=(balls[i].y-balls[i1].y)/(d-balls[i].r+balls[i1].r)
                 }
-                    var b = balls[i].y-m*balls[i].x
-                    balls[i].x += 0.01*(-balls[i1].r-balls[i].r)*Math.sign(balls[i1].x-balls[i].x)
-                    balls[i].y = balls[i].x * m + b
             }
         }
     }
-    }
-
     requestAnimationFrame(main)
 }
 main()
